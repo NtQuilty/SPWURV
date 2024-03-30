77eb364127abd139a3c0227bf3dde781aa6beba2
NTQuilty <onlyhead14@gmail.com>
2024-03-30 10:50:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function Header() {
+	return (
+		<header>
+			<h3>Form</h3>
+
+			<span>Time</span>
+		</header>
+	);
+}
+
+export default function App() {
+
+	return (
+		<div>
+			<Header></Header>
+			<main>
+				<h1>Hello React!</h1>
+			</main>
+		</div>
+	);
+}
+
